e59054d778bdc6c89bf8908ec48afef9a5b62211
polo372 <plbd@laposte.net>
2026-07-01 20:58:08 +0200

    création footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Image from "./Image";
 import Introduction from "./Introduction";
 import Button from "./Button";
 import Section from "./Section"
+import Footer from "./Footer"
 
 export default function App() {
   return(
@@ -10,6 +11,7 @@ export default function App() {
       <Introduction />
       <Button />
       <Section />
+      <Footer />
     </>
   )
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer(){
     return(
         <Footer>
-            <a><img/></a>
-            <a><img/></a>
-            <a><img/></a>
-            <a><img/></a>
-            <a><img/></a>
+            <a><img src="/src/assets/Twitter Icon.png"/></a>
+            <a><img src="/src/assets/Facebook Icon.png"/></a>
+            <a><img src="/src/assets/Instagram Icon.png"/></a>
+            <a><img src="/src/assets/Linkedin Icon.png"/></a>
+            <a><img src="/src/assets/GitHub Icon.png"/></a>
         </Footer>
     )
 }
