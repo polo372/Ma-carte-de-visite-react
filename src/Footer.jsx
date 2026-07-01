d639320e5eefff8241a72fabef177941d6609949
polo372 <plbd@laposte.net>
2026-07-01 21:17:51 +0200

    correction d'erreur de nomage

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,11 +1,11 @@
 export default function Footer(){
     return(
-        <Footer>
+        <footer>
             <a><img className="social twitter" src="/src/assets/Twitter Icon.png"/></a>
             <a><img className="social facebook" src="/src/assets/Facebook Icon.png"/></a>
             <a><img className="social instagram" src="/src/assets/Instagram Icon.png"/></a>
             <a><img className="social linkedin" src="/src/assets/Linkedin Icon.png"/></a>
             <a><img className="social github" src="/src/assets/GitHub Icon.png"/></a>
-        </Footer>
+        </footer>
     )
 }
